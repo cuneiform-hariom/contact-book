e9c61a84c40c79ac79972fc39314eaae43cf102e
hariomcuneiform <hariomcuneiform@gmail.com>
2024-01-05 18:27:37 +0530

    contact book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="title-box p-4">
+        <h2 className='text-center'>My Contact Book</h2>
+      </div>
+      <Form/>
+    </>
+  )
 }
 
-export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,169 @@
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import React, { useEffect, useState } from 'react'
+import pimage from '../assets/profile.webp'
+
+export default function Form() {
+    const [profileImage, setProfileImage] = useState(pimage)
+
+    const [formData, setFormData] = useState([])
+
+    useEffect(() => {
+        console.log('formData: ', formData);
+    }, [formData])
+
+    const initialValues = {
+        name: '',
+        email: '',
+        phone: '',
+        image: null
+    }
+
+    const formSchema = Yup.object().shape({
+        name: Yup.string()
+            .required('required'),
+        email: Yup.string()
+            .email('Invalid email')
+            .required('required'),
+        phone: Yup.string()
+            .matches(/^\d{10}$/, "Invalid phone number")
+            .required("required"),
+        image: Yup.mixed()
+            .required("Image is required")
+    })
+
+    const formik = useFormik({
+        initialValues,
+        validationSchema: formSchema,
+        onSubmit: (values, { resetForm }) => {
+            console.log('values: ', values);
+            resetForm()
+            setProfileImage(pimage)
+            setFormData([...formData, values])
+        },
+    });
+
+    const handleImageChange = async (event) => {
+        const selectedFile = event.target.files[0];
+        formik.setFieldValue("image", selectedFile);
+        setProfileImage(URL.createObjectURL(selectedFile));
+    };
+
+    const handleDelete = (index) => {
+        const newData = formData.filter((data, i) => i !== index);
+        setFormData(newData);
+    };
+
+    return (
+        <>
+            <form className='form p-3' onSubmit={formik.handleSubmit}>
+                <h5 className="text-center">Add Detail</h5>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-7">
+                            <div className='mt-4'>
+                                <label htmlFor="name">Name: {formik.touched.name && formik.errors.name && <span className='text-danger'>{formik.errors.name}</span>}
+                                </label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className='form-control'
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.name}
+                                />
+                            </div>
+                            <div className='mt-4'>
+                                <label htmlFor="email">Email: {formik.touched.email && formik.errors.email && <span className='text-danger'>{formik.errors.email}</span>}
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className='form-control'
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.email}
+                                />
+                            </div>
+                            <div className='mt-4'>
+                                <label htmlFor="phone">Phone: {formik.touched.phone && formik.errors.phone && <span className='text-danger'>{formik.errors.phone}</span>}
+                                </label>
+                                <input
+                                    type="text"
+                                    name="phone"
+                                    id="phone"
+                                    className='form-control'
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.phone}
+                                    maxLength={10}
+                                    onInput={(e) => { e.target.value = e.target.value.slice(0, 10).replace(/\D/g, ""); }}
+                                />
+                            </div>
+
+                        </div>
+                        <div className="col-md-5">
+                            <div className="imgPrev" style={{ backgroundImage: `url(${profileImage})` }}>
+                                <label htmlFor="image"></label>
+                                <input
+                                    type="file"
+                                    name="image"
+                                    id="image"
+                                    className='d-none'
+                                    onChange={handleImageChange}
+                                    onBlur={formik.handleBlur}
+                                    accept='.jpg'
+                                />
+                            </div>
+                            {formik.touched.image && formik.errors.image && <span className='text-danger'>{formik.errors.image}</span>}
+                        </div>
+                        <div className='mt-4'>
+                            <button className='btn btn-primary' type="submit">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+            <div className="allData p-4">
+                <div className="container">
+                    <div className="row">
+                        {
+                            formData.map((data, index) =>
+                                <div className="col-md-4 p-3 s_data" key={index}>
+                                    <div className="border p-2">
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col-md-7">
+                                                    <div className="d-flex align-items-center gap-2">
+                                                        <h4 className='mb-0'>Name:</h4> <span>{data.name}</span>
+                                                    </div>
+                                                    <div className="d-flex align-items-center gap-2">
+                                                        <h4 className='mb-0'>Email:</h4> <span>{data.email}</span>
+                                                    </div>
+                                                    <div className="d-flex align-items-center gap-2">
+                                                        <h4 className='mb-0'>Phone:</h4> <span>{data.phone}</span>
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-5 p-0">
+                                                    <div className="image">
+                                                        <img src={URL.createObjectURL(data.image)} alt="" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="d-flex p-2 gap-3">
+                                            <button className='btn btn-primary btn-sm'>Edit</button>
+                                            <button className='btn btn-primary btn-sm' onClick={() => { handleDelete(index) }}>Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
